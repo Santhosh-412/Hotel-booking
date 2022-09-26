8a94ea8badb343508fde8f800edb92c4555f22fe
Santhosh-412 <114126981+Santhosh-412@users.noreply.github.com>
2022-09-26 17:25:45 +0530

    App.css

---
+++ App.css.py
@@ -0,0 +1,109 @@
+.App {
+    text-align: center;
+}
+
+.App-header {
+    background-color: white;
+    min-height: 100vh;
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: center;
+    font-size: calc(10px + 2vmin);
+    color: black;
+}
+
+.App-link {
+    color: #61dafb;
+}
+
+.table_style {
+    border: 1px solid red;
+    background-color: white;
+    color: blue;
+}
+
+.Header {
+    border: 1px solid Gray;
+    height: 70px;
+    color: black;
+    background-color: #FFD98E;
+    text-align: center;
+    top: 1px;
+    left: 0px;
+    color: #077E8C;
+}
+
+.button1 {
+    position: absolute;
+    top: 160px;
+    left: 450px;
+}
+
+.Rooms1-div1 {
+    position: absolute;
+    top: 290px;
+    left: 1190px
+}
+
+.button2 {
+    position: absolute;
+    top: 400px;
+    left: 850px;
+}
+
+.div-part2 {
+    position: absolute;
+    top: 420px;
+    left: 1190px;
+}
+
+.Div3 {
+    position: absolute;
+    top: 585px;
+    left: 1190px;
+}
+
+.Div4 {
+    position: absolute;
+    top: 120px;
+    left: 1190px;
+}
+
+.submit1 {
+    position: absolute;
+    top: 155px;
+    left: 1530px;
+}
+
+.button2 {
+    position: absolute;
+    top: 405px;
+    left: 850px;
+}
+
+.button3 {
+    position: absolute;
+    top: 565px;
+    left: 1350px;
+}
+
+.button9 {
+    position: absolute;
+    top: 595px;
+    left: 350px;
+}
+
+.submit_button4 {
+    position: absolute;
+    top: 830px;
+    left: 350px;
+}
+
+.submit_button5 {
+    position: absolute;
+    top: 830px;
+    left: 1350px;
+}
+
+
